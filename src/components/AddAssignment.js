951ee03a20400b23c63368ee0815a0812f3445c8
Harsh <hmishra@csumb.edu>
2023-09-28 20:55:19 -0700

    implement feature add student

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,13 +1,113 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { SERVER_URL } from '../constants';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
+function AddAssignment(props) {
+    const [open, setOpen] = useState(false);
+    const [assignmentName, setAssignmentName] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const [courseId, setCourseId] = useState('');
+    const [courseOptions, setCourseOptions] = useState([]);
+    const [message, setMessage] = useState('');
 
-function AddAssignment(props) { 
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
+    const handleClose = () => {
+        setOpen(false);
+        props.onClose();
+    };
 
-  return (
-      <div>
-      </div>
-  ); 
+    const fetchCourseOptions = () => {
+        // Fetch course options from the server
+        fetch(`${SERVER_URL}/courses`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Assuming the data is an array of course objects with 'id' and 'title'
+                setCourseOptions(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching course options: ' + error);
+            });
+    };
+
+    useEffect(() => {
+        // Fetch course options when the component mounts
+        fetchCourseOptions();
+    }, []);
+
+    const handleSubmit = () => {
+        setMessage('');
+        const newAssignment = {
+            assignmentName,
+            dueDate,
+            courseId, // Use the courseId input value as is
+            // handleClose();
+        };
+
+        fetch(`${SERVER_URL}/assignment`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newAssignment),
+        })
+            .then((response) => {
+            // .then((data) => {
+                if (response.ok) {
+                    // Assignment created successfully, clear form fields
+                    setAssignmentName('');
+                    setDueDate('');
+                    setCourseId('');
+                    setMessage('Assignment created successfully.');
+                    handleClose(); // Close the dialog
+                } else {
+                    setMessage('Failed to create assignment: ');
+                }
+            })
+            .catch((error) => {
+                setMessage('Error creating assignment: ' + error);
+            });
+    };
+
+    return (
+        <div>
+            <button onClick={handleOpen}>Add Assignment</button>
+            <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+                <DialogTitle>Add Assignment</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="Assignment Name"
+                        fullWidth
+                        value={assignmentName}
+                        onChange={(e) => setAssignmentName(e.target.value)}
+                    />
+                    <TextField
+                        label="Due Date"
+                        fullWidth
+                        value={dueDate}
+                        onChange={(e) => setDueDate(e.target.value)}
+                    />
+                    <TextField
+                        label="Course ID" // Change the label to "Course ID"
+                        fullWidth
+                        value={courseId}
+                        onChange={(e) => setCourseId(e.target.value)} // Use the input value directly
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSubmit}>Create Assignment</Button>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
 
 export default AddAssignment;
--- src/components/ListAssignment.js
+++ src/components/ListAssignment.js
@@ -1,61 +1,138 @@
-import React, {useState, useEffect} from 'react';
-import {SERVER_URL} from '../constants';
-import {Link} from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
+import { SERVER_URL } from '../constants';
+import { Link } from 'react-router-dom';
+import AddAssignment from './AddAssignment';
+import EditAssignment from './EditAssignment';
 
 function ListAssignment(props) {
+    const [assignments, setAssignments] = useState([]);
+    const [message, setMessage] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalType, setModalType] = useState('');
+    const [selectedAssignment, setSelectedAssignment] = useState(null); // Define selectedAssignment here
 
-  const [assignments, setAssignments] = useState([]);
-  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-   // called once after intial render
-   fetchAssignments();
-  }, [] )
- 
-  const fetchAssignments = () => {
-    console.log("fetchAssignments");
-    fetch(`${SERVER_URL}/assignment`)
-    .then((response) => response.json() ) 
-    .then((data) => { 
-      console.log("assignment length "+data.length);
-      setAssignments(data);
-     }) 
-    .catch(err => console.error(err)); 
-  }
-  
-  
-    const headers = ['Assignment Name', 'Course Title', 'Due Date', ' ', ' ', ' '];
-    
+    useEffect(() => {
+        fetchAssignments();
+    }, []);
+
+    const fetchAssignments = () => {
+        fetch(`${SERVER_URL}/assignment`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setAssignments(data);
+            })
+            .catch((err) => console.error(err));
+    };
+    const refresh = () => {
+        console.log("Coming from dialog")
+        closeModal();
+    }
+
+    const handleDeleteAssignment = (assignmentId) => {
+        fetch(`${SERVER_URL}/assignment/${assignmentId}`, {
+            method: 'DELETE',
+        })
+            .then((response) => {if (response.ok){setMessage('assignment deleted')
+                fetchAssignments();
+
+            }
+            else {setMessage("Assignment delete failed")}
+            }
+            )
+            // .then((data) => {
+            //     if (data.success) {
+            //         const updatedAssignments = assignments.filter((assignment) => assignment.id !== assignmentId);
+            //         setAssignments(updatedAssignments);
+            //         fetchAssignments();
+            //     } else {
+            //         console.error('Failed to delete assignment:', data.error);
+            //     }
+            // })
+            .catch((error) => {
+                console.error('Error deleting assignment:', error);
+            });
+
+    };
+
+    const headers = ['Assignment Name', 'Course Title', 'Due Date', '', '', ''];
+
+    const openModal = (type, assignment) => {
+        setIsModalOpen(true);
+        setModalType(type);
+        setSelectedAssignment(assignment);
+    };
+
+    const closeModal = () => {
+
+        setIsModalOpen(false);
+        setModalType('');
+
+        setSelectedAssignment(null); // Clear the selected assignment
+        console.log("Refreshing");
+        fetchAssignments();
+
+    };
+
     return (
-      <div>
-        <h3>Assignments</h3>
-        <div margin="auto" >
-          <h4>{message}&nbsp;</h4>
-              <table className="Center"> 
-                <thead>
-                  <tr>
-                    {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {assignments.map((row, idx) => (
-                    <tr key={idx}>
-                      <td>{row.assignmentName}</td>
-                      <td>{row.courseTitle}</td>
-                      <td>{row.dueDate}</td>
-                      <td>
-                        <Link to={`/gradeAssignment/${assignments[idx].id}`} >Grade</Link>
-                      </td>
-                      <td>Edit</td>
-                      <td>Delete</td>
+        <div>
+            <h3>Assignments</h3>
+            <div margin="auto">
+                <h4>{message}&nbsp;</h4>
+                <table className="Center">
+                    <thead>
+                    <tr>
+                        {headers.map((title, idx) => (
+                            <th key={idx}>{title}</th>
+                        ))}
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-          </div>
-      </div>
-    )
-}  
+                    </thead>
+                    <tbody>
+                    {assignments.map((row, idx) => (
+                        <tr key={idx}>
+                            <td>{row.assignmentName}</td>
+                            <td>{row.courseTitle}</td>
+                            <td>{row.dueDate}</td>
+                            <td>
+                                <Link to={`/gradeAssignment/${assignments[idx].id}`}>Grade</Link>
+                            </td>
+                            <td>
+                                <button onClick={() => openModal('edit', assignments[idx])}>Edit</button>
+                            </td>
+                            <td>
+                                <button onClick={() => handleDeleteAssignment(assignments[idx].id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                <div>
+                    <button onClick={() => openModal('add', null)}>Add Assignment</button>
+                </div>
+            </div>
+            {isModalOpen && (
+                <div className="add-assignment-modal">
+                    <div className="modal-content">
+                        <button onClick={fetchAssignments} className="close-button">
+                            &times;
+                        </button>
+                        {modalType === 'add' ? (
+                            <div>
+                                <h2>Add Assignment</h2>
+                                <AddAssignment onClose={fetchAssignments} />
+                            </div>
+                        ) : modalType === 'edit' ? (
+                            <div>
+                                <h2>Edit Assignment</h2>
+                                <EditAssignment assignment={selectedAssignment} onClose={refresh} />
+                            </div>
+                        ) : null}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
 
 export default ListAssignment;
